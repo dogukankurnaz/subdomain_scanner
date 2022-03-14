9bd7923e3540253cdfdc8615f8f03e24cabc93db
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2022-03-14 14:06:48 +0300

    Update subdomain-scanner.sh

--- subdomain-scanner.sh
+++ subdomain-scanner.sh
@@ -146,8 +146,10 @@ if [[ $input == "1" ]]; then
 elif [[ $input == "2"  ]]; then
     rm fullscan_subdomainlist.txt;rm fullscan_httpstatus.txt;rm fullscan_results.txt;rm fullscan_subdomainresults.txt
     touch fullscan_subdomainlist.txt;touch fullscan_httpstatus.txt;touch fullscan_curl.txt;touch fullscan_results.txt;touch fullscan_subdomainresults.txt
-    echo -e "${WHITE}+ --------=[*] 1.Fast mode"
-    echo -e "${WHITE}+ --------=[*] 2.FullScan mode (it may take a long time)"
+    echo -e "${WHITE}+ --------=[*] 1.UDP mode"
+    echo -e "${WHITE}+ --------=[*] 2.Subdomains Over Databases (recommended mode)"
+    echo -e "${WHITE}+ --------=[*] 3.FullScan mode (TCP it may take a long time)"
+    
     read -p "Your Choice = " -i Y input
         if [[ $input == "1" ]]; then
         echo -e "${YELLOW}+ -- --=[*] Usage : <domain name>  "
@@ -161,22 +163,42 @@ elif [[ $input == "2"  ]]; then
         do
             host -T $i.$domain | cat >> fast_subdomainlist.txt | $(curl -s -I --http2 http://$i.$domain >> fast_curl.txt | cut -d:  -f1 fast_curl.txt | cat fast_curl.txt | grep HTTP >> fast_httpstatus.txt   | paste fast_subdomainlist.txt fast_httpstatus.txt >> fast_subdomainresults.txt | cat fast_subdomainresults.txt | grep "HTTP" | cut -d ' ' -f1,4,5,6 | sort | uniq -c | sort -nr | sed -e 's/^[ \t]*//' >> fast_results.txt | mv fast_results.txt $(pwd)/${domain})        
         done        
-        fi       
-    
-    echo -e "${PURPLE}+ -- --=[*] Usage : <domain name>  "
-    read -p "Entry Domain =  " -i Y domain
-    mkdir_domain
-    files=${domain} '/'${domain}'_result.txt'
-    echo -e "${WHITE}$(pwd)/${domain}/results.txt file has been created in this directory. If you want EXIT, press ctrl + z"
-    echo -e "${BCYAN} [+] Subdomain Scan Started."
-    progress_bar 1
-    echo -e "${BCYAN} [+] Running in silent mode. If you want EXIT, press ctrl + z and check results.txt in directory."
-    for i in  $(cat wordlist.txt);
-    do
-        host $i.$domain | cat >> fullscan_subdomainlist.txt |$(curl -s -I --http2 http://$i.$domain >> fullscan_curl.txt | cut -d:  -f1 fullscan_curl.txt | cat fullscan_curl.txt | grep HTTP >> fullscan_httpstatus.txt   | paste fullscan_subdomainlist.txt fullscan_httpstatus.txt >> fullscan_subdomainresults.txt | cat fullscan_subdomainresults.txt | grep "HTTP" | cut -d ' ' -f1,4,5,6 | sort | uniq -c | sort -nr | sed -e 's/^[ \t]*//' >> fullscan_results.txt |  mv fullscan_results.txt $(pwd)/${domain} )
-    done
-    
-    
-    
+        fi
+        
 
+        if [[ $input == "2" ]]; then
+        echo -e "${YELLOW}+ -- --=[*] Usage : <domain name>  "
+        read -p "Entry Domain =  " -i Y domain
+        # mkdir_domain
+        # files=${domain} '/' ${domain}'_result.txt'
+        # progress_bar 1
+        curl -s 'https://crt.sh/?q=%.'$domain'&output=json' | jq '.[] | {name_value}' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u |grep "name_value"|cut -d ' ' -f4 >> subdomain_database.txt 
+        echo "[*]-----------------------Crt.sh OK"
+        progress_bar 1
+        curl -s "https://dns.bufferover.run/dns?q=."$domain | jq -r .FDNS_A[]|cut -d',' -f2|sort -u >> subdomain_database.txt
+        echo "[*]----------------------- DNS BufferOver OK"
+        progress_bar 1
+        curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | grep -o -E "[a-zA-Z0-9._-]+\.$domain" >> subdomain_database.txt
+        echo "[*]----------------------- AlienVault OK"
+        progress_bar 1
+        curl -s "https://urlscan.io/api/v1/search/?q=$domain" | grep -o -E "[a-zA-Z0-9._-]+\.$domain"  >> subdomain_database.txt       
+        echo "[*]----------------------- URLSCAN OK"
+        cat subdomain_database.txt | sort | uniq -c | sort -nr | sed -e 's/^[ \t]*//' > subdomain_database.txt
+        progress_bar 1        
+        fi        
+    
+        if [[ $input == "3" ]]; then
+            echo -e "${PURPLE}+ -- --=[*] Usage : <domain name>  "
+            read -p "Entry Domain =  " -i Y domain
+            mkdir_domain
+            files=${domain} '/'${domain}'_result.txt'
+            echo -e "${WHITE}$(pwd)/${domain}/results.txt file has been created in this directory. If you want EXIT, press ctrl + z"
+            echo -e "${BCYAN} [+] Subdomain Scan Started."
+            progress_bar 1
+            echo -e "${BCYAN} [+] Running in silent mode. If you want EXIT, press ctrl + z and check results.txt in directory."
+            for i in  $(cat wordlist.txt);
+            do
+                host $i.$domain | cat >> fullscan_subdomainlist.txt |$(curl -s -I --http2 http://$i.$domain >> fullscan_curl.txt | cut -d:  -f1 fullscan_curl.txt | cat fullscan_curl.txt | grep HTTP >> fullscan_httpstatus.txt   | paste fullscan_subdomainlist.txt fullscan_httpstatus.txt >> fullscan_subdomainresults.txt | cat fullscan_subdomainresults.txt | grep "HTTP" | cut -d ' ' -f1,4,5,6 | sort | uniq -c | sort -nr | sed -e 's/^[ \t]*//' >> fullscan_results.txt |  mv fullscan_results.txt $(pwd)/${domain} )
+            done
+        fi       
 fi
